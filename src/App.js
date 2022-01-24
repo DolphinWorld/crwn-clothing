48ae36d7a6b4cc2674b2718b5a8adf0f62878326
Jack Su <jacksu@fb.com>
2022-01-23 19:25:25 -0800

    added header

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@
 import './App.css';
 import HomePage from './pages/homepage/homepage.component';
 import ShopPage from './pages/shop/shoppage.component';
+import Header from './components/header/header.component';
 
 import { Switch, Route } from 'react-router-dom';
 
@@ -14,6 +15,7 @@ const HatsPage = () => (
 function App() {
   return (
     <div className="App">
+      <Header />
       <Switch>
         <Route exact path="/" component={ HomePage } />
         <Route exact path="/shop" component={ ShopPage } />
